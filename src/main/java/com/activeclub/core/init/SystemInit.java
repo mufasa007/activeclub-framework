9ff97580d165997ee01364523968f79e281b3523
Mufasa007 <594564264@qq.com>
2021-11-21 20:41:49 +0800

    [CHG] SystemInit

--- src/main/java/com/activeclub/core/init/SystemInit.java
+++ src/main/java/com/activeclub/core/init/SystemInit.java
@@ -8,10 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 /**
  * @Author 59456
  * @Date 2021/11/2
- * @Descrip
+ * @Descrip 系统启动前、启动后的初始化操作
  * @Version 1.0
  */
-public interface SystemInit extends InitializingBean, CommandLineRunner {
-
+public abstract class SystemInit implements InitializingBean, CommandLineRunner {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 }
